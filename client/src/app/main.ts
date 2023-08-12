24aa278787af99bb5bbf75d21fe972fcb6031b3f
Denis Laluev <laluevdenis@yandex.ru>
2023-08-12 19:59:14 +0300

    add the '@' alias

--- client/src/app/main.ts
+++ client/src/app/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import { Home } from "../pages/home";
+import { Home } from "@/pages/home";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [{ path: "/", component: Home }];
