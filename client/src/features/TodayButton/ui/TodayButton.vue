41041ba178e6ea950d2e6eaf107813ffd9c85b8d
Denis Laluev <laluevdenis@yandex.ru>
2024-01-18 17:19:20 +0300

    Add redirect when clicking on the TodayButton

--- client/src/features/TodayButton/ui/TodayButton.vue
+++ client/src/features/TodayButton/ui/TodayButton.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { router } from "@/app/main";
 import { Button } from "@/shared/ui";
+import { formatDateForURL } from "@/shared/utils";
+
+const handleRedirect = () => {
+  router.push("./day/" + formatDateForURL(new Date()));
+};
 </script>
 
 <template>
-  <Button>Jump to today's note</Button>
+  <Button @click="handleRedirect">Jump to today's note</Button>
 </template>
--- client/src/shared/ui/button/Button.vue
+++ client/src/shared/ui/button/Button.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "click");
+}>();
+</script>
 
 <template>
   <button
+    @click="$emit('click')"
     class="text-center font-semibold bg-main-800 text-white py-[0.675rem] px-5 rounded-md"
   >
     <slot></slot>
